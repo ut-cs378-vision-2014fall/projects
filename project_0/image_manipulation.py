4ee75a3b3267111a73dd0cae5642787efdb53650
Bryan Klingner <github.b@overt.org>
2014-08-26 21:03:49 -0500

    Add project_0 files.

---
+++ project_0/image_manipulation.py
@@ -0,0 +1,11 @@
+"""Assignment 1: Image Manipulation with OpenCV.
+
+In this assignment, you will implement a few basic image
+manipulation tasks using the OpenCV library.
+
+Use the unit tests is image_manipulation_test.py to guide
+your implementation, adding functions as needed until all
+unit tests pass.
+"""
+
+# TODO: Implement!
---
+++ project_0/image_manipulation_test.py
@@ -0,0 +1,75 @@
+"""Unit tests for the image_manipulation module.
+
+Implement your image_manipulation module so that all tests pass.
+An example of how to run these tests is given in run_tests.sh.
+"""
+
+import cv2
+import image_manipulation
+import numpy
+import unittest
+
+
+def _sparse_pixels(image, stride=50):
+    """Returns pixels sparsely sampled by 'stride' in 'image.'"""
+    rows, cols, _ = image.shape
+    for row in range(0, rows, stride):
+        for col in range(0, cols, stride):
+            yield row, col
+
+
+class TestImageManipulation(unittest.TestCase):
+    """Tests the functionality of the image_manipulation module."""
+
+    def setUp(self):
+        """Initializes shared state for unit tests."""
+        self.nyc_image = cv2.imread("test_data/nyc.jpg")
+
+    def _assert_pixels_equal(self, row, col, expected_pixel, actual_pixel):
+        """Asserts deep equality between expected_pixel and actual_pixel."""
+        self.assertTrue(
+            numpy.array_equal(expected_pixel, actual_pixel),
+            "Expected pixel (%d, %d) = %s, actual %s" % (
+                col, row, expected_pixel, actual_pixel))
+
+    def test_flip_image(self):
+        """Tests the flip_image function."""
+
+        # Test each combination of horizontal and vertical flipping.
+        for horizontal in (False, True):
+            for vertical in (False, True):
+                nyc_flipped = image_manipulation.flip_image(
+                    self.nyc_image, horizontal, vertical)
+
+                rows, cols, _ = self.nyc_image.shape
+                for row, col in _sparse_pixels(self.nyc_image):
+                    row_flipped = rows - row - 1 if vertical else row
+                    col_flipped = cols - col - 1 if horizontal else col
+                    expected_pixel = self.nyc_image[row_flipped, col_flipped]
+                    self._assert_pixels_equal(
+                        row, col, expected_pixel, nyc_flipped[row, col])
+
+    def test_negate_image(self):
+        """Tests the invert_image function."""
+
+        nyc_negated = image_manipulation.negate_image(self.nyc_image)
+
+        for row, col in _sparse_pixels(self.nyc_image):
+            expected_pixel = [255, 255, 255] - self.nyc_image[row, col]
+            self._assert_pixels_equal(
+                row, col, expected_pixel, nyc_negated[row, col])
+
+    def test_swap_red_and_green(self):
+        """Tests the swap_red_and_green function."""
+        nyc_swapped = image_manipulation.swap_red_and_green(self.nyc_image)
+
+        for row, col in _sparse_pixels(self.nyc_image):
+            expected_pixel = []
+            expected_pixel.append(self.nyc_image[row, col, 1])  # First green.
+            expected_pixel.append(self.nyc_image[row, col, 0])  # Then red.
+            expected_pixel.append(self.nyc_image[row, col, 2])  # Then blue.
+            self._assert_pixels_equal(
+                row, col, expected_pixel, nyc_swapped[row, col])
+
+if __name__ == '__main__':
+    unittest.main()
